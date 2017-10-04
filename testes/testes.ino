f4aec8710733619a1ad01977e322818670816f15
russijunior <junior.russi2011@gmail.com>
2017-10-04 20:20:40 -0300

    Many buttons

--- testes/testes.ino
+++ testes/testes.ino
@@ -19,13 +19,31 @@ int intervaloPosicao = 180;
 
 bool motorLiberado = true;
 
-int botao = 12;
+int botao = 40;
 bool botaoDesativado = false;
+int botao1 = 41;
+bool botaoDesativado1 = false;
+int botao2 = 42;
+bool botaoDesativado2 = false;
+int botao3 = 43;
+bool botaoDesativado3 = false;
+int botao4 = 44;
+bool botaoDesativado4 = false;
+int botao5 = 45;
+bool botaoDesativado5 = false;
+int botao6 = 46;
+bool botaoDesativado6 = false;
 
-int botaoBraco = 11;
+int botaoBraco = 38;
 
 void setup() {
   pinMode(botao, INPUT);
+  pinMode(botao1, INPUT);
+  pinMode(botao2, INPUT);
+  pinMode(botao3, INPUT);
+  pinMode(botao4, INPUT);
+  pinMode(botao5, INPUT);
+  pinMode(botao6, INPUT);
   pinMode(botaoBraco, INPUT);
   
   s.attach(SERVO);
@@ -82,13 +100,30 @@ void atualizarMotor()
 void pressionarBotao()
 {
   bool leituraBotao = digitalRead(botao);
-  
   botaoDesativado = leituraBotao;
+
+  bool leituraBotao1 = digitalRead(botao1);
+  botaoDesativado1 = leituraBotao1;
+
+  bool leituraBotao2 = digitalRead(botao2);
+  botaoDesativado2 = leituraBotao2;
+
+  bool leituraBotao3 = digitalRead(botao3);
+  botaoDesativado3 = leituraBotao3;
+
+  bool leituraBotao4 = digitalRead(botao4);
+  botaoDesativado4 = leituraBotao4;
+
+  bool leituraBotao5 = digitalRead(botao5);
+  botaoDesativado5 = leituraBotao5;
+
+  bool leituraBotao6 = digitalRead(botao6);
+  botaoDesativado6 = leituraBotao6;
 }
 
 void alterarPosicaoServo(int intervaloReal)
 {
-  if (!motorLiberado || botaoDesativado)
+  if (!motorLiberado || botaoDesativado || botaoDesativado1 || botaoDesativado2 || botaoDesativado3 || botaoDesativado4 || botaoDesativado5 || botaoDesativado6)
     return;
   
   Serial.println("alterarPosicaoServo");
